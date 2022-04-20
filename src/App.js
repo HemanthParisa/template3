921fed0f52f72bb968cc57366c65cd0a413ddbe7
Hemanth Parisa <hemanthkumarparisa@gmail.com>
2022-04-20 15:21:47 +0530

    firstcommit

---
+++ src/App.js
@@ -0,0 +1,65 @@
+import React from "react";
+import "./App.css";
+import hemanth from "./images/download.png"; 
+
+const finalcomponent = () =>{
+
+    return(
+         <div>
+        <div className="task">
+            <div className="task1">
+                <div>Logo</div>
+            </div>
+            <div className="task11">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+            </div>
+
+            <div className="local">
+                <div>
+                    <div className="l1"><img src={hemanth} style={{ height:"50px",width:"20%"}} /></div>                    
+                    <div>INTRODUCTION CONTENT</div>
+                    <div>Buttons</div> 
+                    </div>
+            </div>
+            <div className="what">
+                <div>WHAT WE DO </div>
+                <div>CONTENT</div>
+            </div>
+            <div className="hemanth">
+                <div className="icon1">icon1</div>
+                <div className="icon2">icon2</div>
+                <div className="icon3">icon3</div>
+                <div className="icon4">icon4</div>
+                <div className="icon5">icon5</div>
+            </div>
+            <div className="form">
+            <div className="form1">
+                <div>Form Request</div>
+            </div>
+            <div className="form11">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+            </div>
+            <div className="news">
+                <div>LATEST NEWS</div>
+                <div>LATEST NEWS</div>
+            </div>
+            <div className="link">
+                <div>LINKS</div>
+            </div>
+            
+            
+        
+
+    </div>
+    );
+    
+}
+export default finalcomponent;
